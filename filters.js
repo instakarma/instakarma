23b489ab6f1b0a6d2555c70177f4765fa5e7ed0e
Rune Halvorsen <runefh@gmail.com>
2015-04-14 00:33:46 +0200

    Added fallback avatar image

--- filters.js
+++ filters.js
@@ -6,7 +6,11 @@ const env = new nunjucks.Environment();
 const filterMap = {
 	fuzzyTimeSinceNow(date) {
 		return moment(date).fromNow();
-	}
+	},
+
+    avatarUrl(user) {
+        return user.avatar || 'http://api.adorable.io/avatars/50/' + user.id + '.png'
+    }
 }
 
 module.exports = filterMap;
